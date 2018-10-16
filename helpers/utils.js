dcd32ee531f3df1b810442f7eaec778a4dacdf62
vianvi <30834324+vianvi@users.noreply.github.com>
2018-10-16 10:23:12 +0100

    pass env variables from jenkins

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,18 +1,15 @@
 const getTestUserLoginDetails = () => {
-    if (!process.env.CCD_E2E_EMAIL || !process.env.CCD_E2E_PASSWORD) {
-        throw new Error('You need to set CCD_E2E_EMAIL and CCD_E2E_PASSWORD env variables');
+    if (!process.env.CCD_E2E_PASSWORD) {
+        throw new Error('You need to set CCD_E2E_PASSWORD env variable');
     }
-    return { 
-        username: process.env.CCD_E2E_EMAIL ,
+    return {
+        username: 'michael.osinloye@hmcts.net' ,
         password: process.env.CCD_E2E_PASSWORD
     };
 }
 
 const getBaseUrl = () => {
-    if (!process.env.TEST_ENV) {
-        throw new Error('You need to set TEST_ENV variable e.g. www-ccd.nonprod.platform.hmcts.net');
-    }
-    return process.env.TEST_ENV;
+    return 'www-ccd.nonprod.platform.hmcts.net';
 }
 
 module.exports = {
