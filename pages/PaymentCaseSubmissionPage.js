5086b3dfb0efea335e18fb5cfcdf2d47bd7f4dd8
mounikahmcts <mounika.ammineni1@hmcts.net>
2020-11-11 13:51:22 +0000

    create case

--- pages/PaymentCaseSubmissionPage.js
+++ pages/PaymentCaseSubmissionPage.js
@@ -16,7 +16,7 @@ module.exports = {
     I.selectOption(this.fields.howPaymentMade, 'Fee account');
     I.click(this.fields.submit);
     //I.fillField(this.fields.enterYourAccountNumber, 'PBA1234908');
-    I.selectOption(this.fields.selectPbaNumber, I.grabTextFrom('select option:nth-child(2)'));
+    I.selectOption(this.fields.selectPbaNumber, 'PBA0072999');
     I.fillField(this.fields.enterYourReference, 'Next case submitted');
     I.click(this.fields.submit);
     I.wait(1);
