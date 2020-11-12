07a67f6c35e8e9eed8267e7cf25d307847fc65b8
mounikahmcts <mounika.ammineni1@hmcts.net>
2020-11-12 09:23:02 +0000

    pbaNumber updated

--- pages/PaymentCaseSubmissionPage.js
+++ pages/PaymentCaseSubmissionPage.js
@@ -16,16 +16,18 @@ module.exports = {
     I.selectOption(this.fields.howPaymentMade, 'Fee account');
     I.click(this.fields.submit);
     //I.fillField(this.fields.enterYourAccountNumber, 'PBA1234908');
-    const pbaNumbers = await I.grabTextFromAll(`${this.fields.selectPbaNumber} option`);
-    const pbaNumbers1 = await I.grabValueFrom(`${this.fields.selectPbaNumber} option`);
-    const pbaNumbers2 = await I.grabValueFromAll(`${this.fields.selectPbaNumber} option`);
-    const pbaNumber = await I.grabTextFrom(`${this.fields.selectPbaNumber} option:nth-child(2)`);
+    const pbaNumbers = await I.grabTextFromAll(this.fields.selectPbaNumber);
+    console.log('the values are ..',  pbaNumbers);
 
+    const pbaNumbers1 = await I.grabValueFrom(this.fields.selectPbaNumber);
     console.log('the values are1 ..',  pbaNumbers1);
+
+    const pbaNumbers2 = await I.grabValueFromAll(this.fields.selectPbaNumber);
     console.log('the values are2 ..',  pbaNumbers2);
 
-    console.log('the values are ..',  pbaNumbers);
+    const pbaNumber = await I.grabTextFrom(`${this.fields.selectPbaNumber} option:nth-child(2)`);
     console.log('the value is ..',  pbaNumber);
+    
     I.selectOption(this.fields.selectPbaNumber, pbaNumber);
     I.fillField(this.fields.enterYourReference, 'Next case submitted');
     I.click(this.fields.submit);
