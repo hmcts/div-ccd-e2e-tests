f02e5e83b0ae61b52f81d7040bff6a00c92d909e
mounikahmcts <mounika.ammineni1@hmcts.net>
2020-11-12 09:06:09 +0000

    pbaNumber updated

--- pages/PaymentCaseSubmissionPage.js
+++ pages/PaymentCaseSubmissionPage.js
@@ -16,10 +16,15 @@ module.exports = {
     I.selectOption(this.fields.howPaymentMade, 'Fee account');
     I.click(this.fields.submit);
     //I.fillField(this.fields.enterYourAccountNumber, 'PBA1234908');
-    const pbaNumber =  I.grabTextFrom(`${this.fields.selectPbaNumber} option:nth-child(2)`)
-                          .then((data) =>{
-                            return data
-                          });
+    const pbaNumbers = await I.grabTextFromAll(`${this.fields.selectPbaNumber} option`);
+    const pbaNumbers1 = await I.grabValueFrom(`${this.fields.selectPbaNumber} option`);
+    const pbaNumbers2 = await I.grabValueFromAll(`${this.fields.selectPbaNumber} option`);
+    const pbaNumber = await I.grabTextFrom(`${this.fields.selectPbaNumber} option:nth-child(2)`);
+
+    console.log('the values are1 ..',  pbaNumbers1);
+    console.log('the values are2 ..',  pbaNumbers2);
+
+    console.log('the values are ..',  pbaNumbers);
     console.log('the value is ..',  pbaNumber);
     I.selectOption(this.fields.selectPbaNumber, pbaNumber);
     I.fillField(this.fields.enterYourReference, 'Next case submitted');
