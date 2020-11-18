803f698b189d410a2bb2e505bfe8dd91f0739271
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2020-11-18 08:47:26 +0000

    Desertion cases updated (#33)

--- tests/verifyDesertionCase.js
+++ tests/verifyDesertionCase.js
@@ -5,16 +5,14 @@ const verifyContent = require('../data/ccdDesertionCase.json');
 const caseWorker = getCaseWorkerLoginDetails();
 const reason = 'Desertion';
 
+let caseId;
+
 Feature('verify desertion case ');
 
-Scenario('Desertion case', async function (I) {
-  const caseId = await createCaseInCcd('data/ccdDesertionCase.json');
+Scenario('Desertion case - Petitioner', async function (I) {
+  caseId = await createCaseInCcd('data/ccdDesertionCase.json');
   const response = await updateCaseInCcd(caseId, 'hwfApplicationAcceptedfromAwaitingHWFDecision');
   const submitted = await updateCaseInCcd(caseId, 'issueFromSubmitted');
-  const issueAOS = await updateCaseInCcd(caseId, 'issueAos');
-  const startAOS = await updateCaseInCcd(caseId, 'startAos');
-  const aosSubmittedUndefended = await updateCaseInCcd(caseId, 'aosSubmittedUndefended');
-  const dnApplied = await updateCaseInCcd(caseId, 'dnReceived');
 
   I.amOnHomePage();
   I.login(caseWorker.username, caseWorker.password);
@@ -22,10 +20,36 @@ Scenario('Desertion case', async function (I) {
   I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
   I.wait(30);
   I.verifyPetitionerTab(reason, verifyContent);
-  I.verifyDNAnswersInTab(reason, verifyContent);
-  I.verifyAOSAnswersInTab(reason, verifyContent);
-  I.verifyDocumentsTab(reason, caseId);
   I.verifyConfidentialPetitionerTab(verifyContent);
   I.verifyMarriageCertificateTab(verifyContent);
+  I.click('Sign out');
+});
+
+Scenario('Desertion case - AOS', async function (I) {
+
+  console.log('caseId is', caseId);
+  const issueAOS = await updateCaseInCcd(caseId, 'issueAos');
+  const startAOS = await updateCaseInCcd(caseId, 'startAos');
+  const aosSubmittedUndefended = await updateCaseInCcd(caseId, 'aosSubmittedUndefended');
+
+  I.amOnHomePage();
+  I.login(caseWorker.username, caseWorker.password);
+  I.wait(20);
+  I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
+  I.wait(30);
+  I.verifyAOSAnswersInTab(reason, verifyContent);
+  I.click('Sign out');
+});
+
+Scenario('Desertion case - DN', async function (I) {
+  const dnApplied = await updateCaseInCcd(caseId, 'dnReceived');
+
+  I.amOnHomePage();
+  I.login(caseWorker.username, caseWorker.password);
+  I.wait(20);
+  I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
+  I.wait(30);
+  I.verifyDNAnswersInTab(reason, verifyContent);
+  I.verifyDocumentsTab(reason, caseId);
   I.click('Sign out');
 });
