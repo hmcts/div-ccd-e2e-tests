082bc01c8ba498f42b300775da9bf2f5614c41cd
mounikahmcts <mounika.ammineni1@hmcts.net>
2020-11-18 09:26:16 +0000

    Refactored code

--- tests/verifyAdulteryCase.js
+++ tests/verifyAdulteryCase.js
@@ -3,30 +3,63 @@
 const { createCaseInCcd, updateCaseInCcd, getCaseWorkerLoginDetails } = require('../helpers/utils');
 const verifyContent = require('../data/ccdAdulteryRespondentCorespondentDefendedCase.json');
 const caseWorker = getCaseWorkerLoginDetails();
+const { reasonsForDivorce, signOut } = require('../common/constants');
 
-Feature('verify Adutlery case ');
+let caseId;
 
-Scenario('Adultery case defended by both Resp and Co Resp', async function (I) {
-  const caseId = await createCaseInCcd('data/ccdAdulteryRespondentCorespondentDefendedCase.json');
+Feature('Verify Adultery case defended by both Resp and Co Res ');
+
+Scenario('Adultery case - Petitioner', async function (I) {
+  caseId = await createCaseInCcd('data/ccdAdulteryRespondentCorespondentDefendedCase.json');
   const response = await updateCaseInCcd(caseId, 'hwfApplicationAcceptedfromAwaitingHWFDecision');
   const submitted = await updateCaseInCcd(caseId, 'issueFromSubmitted');
-  const issueAOS = await updateCaseInCcd(caseId, 'issueAos');
-  const startAOS = await updateCaseInCcd(caseId, 'startAos');
-  const aosSubmittedCoRespoDefended = await updateCaseInCcd(caseId, 'co-RespAOSReceivedStarted');
-  const aosSubmittedRespoDefended = await updateCaseInCcd(caseId, 'aosSubmittedDefended');
-  const coRespAnswerReceivedForDefended = await updateCaseInCcd(caseId,'coRespAnswerReceivedAOS');
-  const reason ='Adultery';
 
   I.amOnHomePage();
   I.login(caseWorker.username, caseWorker.password);
   I.wait(20);
   I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
   I.wait(30);
-  I.verifyPetitionerTab(reason, verifyContent);
-  I.verifyAOSAnswersInTab(reason, verifyContent);
-  I.verifyDocumentsTab(reason, caseId);
+  I.verifyPetitionerTab(reasonsForDivorce.ADULTERY, verifyContent);
+  I.verifyConfidentialPetitionerTab(verifyContent);
   I.verifyMarriageCertificateTab(verifyContent);
-  I.verifyCorespondentTab(verifyContent);
-  I.click('Sign out');
+  I.click(signOut);
+});
 
+Scenario('Adultery case  - AOS ', async function (I) {
+  const issueAOS = await updateCaseInCcd(caseId, 'issueAos');
+  const startAOS = await updateCaseInCcd(caseId, 'startAos');
+  const aosSubmittedCoRespoDefended = await updateCaseInCcd(caseId, 'co-RespAOSReceivedStarted');
+
+  I.amOnHomePage();
+  I.login(caseWorker.username, caseWorker.password);
+  I.wait(20);
+  I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
+  I.wait(30);
+  I.verifyAOSAnswersInTab(reasonsForDivorce.ADULTERY, verifyContent);
+  I.click(signOut);
+});
+
+Scenario('Adultery case - CoResp ', async function (I) {
+  const coRespAnswerReceivedForDefended = await updateCaseInCcd(caseId,'coRespAnswerReceivedAOS');
+
+  I.amOnHomePage();
+  I.login(caseWorker.username, caseWorker.password);
+  I.wait(20);
+  I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
+  I.wait(30);
+  I.verifyCorespondentTab(verifyContent);
+  I.click(signOut);
+});
+
+Scenario('Adultery case  - DN', async function (I) {
+  const dnApplied = await updateCaseInCcd(caseId, 'dnReceived');
+
+  I.amOnHomePage();
+  I.login(caseWorker.username, caseWorker.password);
+  I.wait(20);
+  I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
+  I.wait(30);
+  I.verifyDNAnswersInTab(reasonsForDivorce.ADULTERY, verifyContent);
+  I.verifyDocumentsTab(reasonsForDivorce.ADULTERY, caseId);
+  I.click(signOut);
 });
--- tests/verifyBehaviourCase.js
+++ tests/verifyBehaviourCase.js
@@ -3,17 +3,43 @@
 const { createCaseInCcd, updateCaseInCcd, getCaseWorkerLoginDetails, firstLetterToCaps, datechange } = require('../helpers/utils');
 const verifyContent = require('../data/ccdBehaviourUnDefendedCase.json');
 const caseWorker = getCaseWorkerLoginDetails();
-const reason = 'Behaviour';
+const { reasonsForDivorce, signOut } = require('../common/constants');
 
-Feature('verify behaviour case ');
+let caseId;
 
-Scenario('Behaviour case Undefended', async function (I) {
-  const caseId = await createCaseInCcd('data/ccdBehaviourUnDefendedCase.json');
+Feature('Verify Behaviour Case ');
+
+Scenario('Behaviour case Undefended - Petitioner', async function (I) {
+  caseId = await createCaseInCcd('data/ccdBehaviourUnDefendedCase.json');
   const response = await updateCaseInCcd(caseId, 'hwfApplicationAcceptedfromAwaitingHWFDecision');
   const submitted = await updateCaseInCcd(caseId, 'issueFromSubmitted');
+
+  I.amOnHomePage();
+  I.login(caseWorker.username, caseWorker.password);
+  I.wait(20);
+  I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
+  I.wait(30);
+  I.verifyPetitionerTab(reasonsForDivorce.BEHAVIOUR, verifyContent);
+  I.verifyConfidentialPetitionerTab(verifyContent);
+  I.verifyMarriageCertificateTab(verifyContent);
+  I.click(signOut);
+});
+
+Scenario('Behaviour case Undefended - AOS', async function (I) {
   const issueAOS = await updateCaseInCcd(caseId, 'issueAos');
   const startAOS = await updateCaseInCcd(caseId, 'startAos');
   const aosSubmittedUndefended = await updateCaseInCcd(caseId, 'aosSubmittedUndefended');
+
+  I.amOnHomePage();
+  I.login(caseWorker.username, caseWorker.password);
+  I.wait(20);
+  I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
+  I.wait(30);
+  I.verifyAOSAnswersInTab(reasonsForDivorce.BEHAVIOUR, verifyContent);
+  I.click(signOut);
+});
+
+Scenario('Behaviour case Undefended - DN', async function (I) {
   const dnApplied = await updateCaseInCcd(caseId, 'dnReceived');
 
   I.amOnHomePage();
@@ -21,11 +47,7 @@ Scenario('Behaviour case Undefended', async function (I) {
   I.wait(20);
   I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
   I.wait(30);
-  I.verifyPetitionerTab(reason, verifyContent);
-  I.verifyDNAnswersInTab(reason, verifyContent);
-  I.verifyAOSAnswersInTab(reason, verifyContent);
-  I.verifyDocumentsTab(reason, caseId);
-  I.verifyConfidentialPetitionerTab(verifyContent);
-  I.verifyMarriageCertificateTab(verifyContent);
-  I.click('Sign out');
+  I.verifyDNAnswersInTab(reasonsForDivorce.BEHAVIOUR, verifyContent);
+  I.verifyDocumentsTab(reasonsForDivorce.BEHAVIOUR, caseId);
+  I.click(signOut);
 });
