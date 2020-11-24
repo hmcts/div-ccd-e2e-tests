207cbc722f0152ed6b2f0aabea22d5588345ed7f
mounikahmcts <43175082+mounikahmcts@users.noreply.github.com>
2020-11-24 15:56:30 +0000

    Ta 171 desertion dn (#44)
    * Desertion cases updated
    
    * Refactored code
    
    * update tests
    
    * add tests
    
    * refactored code
    
    * refactor
    
    * refactor
    
    * updated new events
    
    * update test case
    
    * tests updated
    
    * updated
    
    * update solciitor cases
    
    * update solciitor cases
    
    * duplicate
    
    * separation reasons test data
    
    * update separation tests
    
    * updated tests
    
    * update test case
    
    * remove console log
    
    * remove console log
    
    * update case
    
    * update case
    
    * update case
    
    * update case
    
    * update tests
    
    * update case
    
    * update case
    
    * update case

---
+++ tests/verifyLinkedCase.js
@@ -0,0 +1,20 @@
+/// <reference path="../steps.d.ts" />
+
+const { createCaseInCcd, getCaseWorkerLoginDetails } = require('../helpers/utils');
+const caseWorker = getCaseWorkerLoginDetails();
+const { signOut } = require('../common/constants');
+
+
+Feature('Verify Amended Case ');
+
+Scenario('Create linked case and verify tab', async function (I) {
+  const caseId = await createCaseInCcd('data/ccdLinkedCase.json');
+  I.amOnHomePage();
+  I.login(caseWorker.username, caseWorker.password);
+  I.wait(20);
+  I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
+  I.wait(30);
+  I.validateLinkedCaseTabData();
+  I.click(signOut);
+});
+
