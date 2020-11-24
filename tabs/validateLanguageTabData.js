97ed5663111c77bb62aa410d76a4fa292fb6a020
mounikahmcts <mounika.ammineni1@hmcts.net>
2020-11-24 12:16:23 +0000

    update case

--- tabs/validateLanguageTabData.js
+++ tabs/validateLanguageTabData.js
@@ -6,8 +6,8 @@ module.exports = (verifyContent) => {
     I.click(labels.name);
     I.see(labels.prefWelsh);
     I.see(firstLetterToCaps(verifyContent.LanguagePreferenceWelsh));
-    I.see(labels.changesToPetAtDN);
     I.see(labels.livingArrangementsDetails);
+    I.see(verifyContent.DesertionTimeLivedTogetherDetailsDNTrans);
     I.see(labels.stateWhatPetClaim);
     I.see(verifyContent.CostsDifferentDetailsTrans);
 }
