3ea23f607fae84ad898b7cda2ca24afb5aa7eb52
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-01-30 17:11:32 +0000

    update doc upload

--- pages/UploadMarriageCertificatePage.js
+++ pages/UploadMarriageCertificatePage.js
@@ -14,6 +14,7 @@ module.exports = {
     await I.click(this.fields.addNewButton);
     await I.wait(1);
     await I.attachFile('input[id="D8DocumentsUploaded_0_DocumentLink"]', 'data/fileupload.txt');
+    await I.wait(1);
     await I.waitForVisible(this.fields.submit);
     await I.click(this.fields.submit);
   }
