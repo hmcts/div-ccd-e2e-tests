3c570ea54b957a179f52bee7adc8920fa7706761
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-01-30 20:33:15 +0000

    update file upload

--- pages/UploadMarriageCertificatePage.js
+++ pages/UploadMarriageCertificatePage.js
@@ -4,7 +4,8 @@ module.exports = {
 
   fields: {
     addNewButton: 'button[type="button"]',
-    chooseFile: '#D8DocumentsUploaded_0_DocumentLink',
+    chooseFile: 'input[id="D8DocumentsUploaded_0_DocumentLink"]',
+    fileComment: '#D8DocumentsUploaded_0_DocumentComment',
     submit: 'button[type="submit"]'
   },
 
@@ -13,7 +14,8 @@ module.exports = {
     await I.see('Documents uploaded (Optional)');
     await I.click(this.fields.addNewButton);
     await I.wait(1);
-    await I.attachFile('input[id="D8DocumentsUploaded_0_DocumentLink"]', 'data/fileupload.txt');
+    await I.attachFile(this.fields.chooseFile, 'data/fileupload.txt');
+    await I.fillField(this.fields.fileComment, 'Uploading a dummy file');
     await I.wait(1);
     await I.waitForVisible(this.fields.submit);
     await I.click(this.fields.submit);
