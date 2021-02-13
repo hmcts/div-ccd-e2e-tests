e81e2a0faa9ef8d349ec0f79bfbccb4a0f5702f2
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-02-13 15:38:03 +0000

    update retries

--- bin/run-crossbrowser-tests.sh
+++ bin/run-crossbrowser-tests.sh
@@ -5,7 +5,7 @@ export TESTS_FOR_CROSS_BROWSER='true'
 export SMART_WAIT=90000
 export WAIT_FOR_TIMEOUT=90000
 export E2E_OUTPUT_DIR='./functional-output/xui/crossbrowser'
-export RETRY_SCENARIOS=0
+export RETRY_SCENARIOS=3
 
 if [[ "$BROWSER_GROUP" == "" ]]
 then
