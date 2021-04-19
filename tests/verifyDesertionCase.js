5ca85eafe797a030ceecd6d6c92f158b03853623
Dale Cotterill <dalec@kainos.com>
2021-04-19 13:06:25 +0100

    Add log in steps to verify tab field tests

--- tests/verifyDesertionCase.js
+++ tests/verifyDesertionCase.js
@@ -53,6 +53,8 @@ Scenario('Execute events for end to end flow of PFE, RFE, DN , DA', async functi
   verifyState(daGranted, states.DIVORCE_GRANTED);
 
   // verify all tab fields of PFE, RFE, DN, DA
+  await I.amOnHomePage();
+  await I.login(testconfig.TestEnvCWUser, testconfig.TestEnvCWPassword);
   await I.wait(1);
   await I.amOnPage('/case/DIVORCE/DIVORCE/' + caseId);
   await I.wait(1);
