7e0b127e370cc3a24432fd1154e9921545c7e14f
Patryk Jar <jar.patryk@gmail.com>
2021-04-19 21:02:34 +0200

    This can be green

--- tests/solicitorhelpwithfees-sep5yr.js
+++ tests/solicitorhelpwithfees-sep5yr.js
@@ -40,7 +40,7 @@ Scenario('Solicitor create case and make payment', async (I) => {
   await I.solAwaitingPaymentConfPageFormAndSubmit();
 
   // Solicitor should not see issue, refund events
-  await I.amOnPage('/cases/case-details/' + caseNumber);
+  await I.amOnPage('/case/DIVORCE/DIVORCE/' + caseNumber);
   await I.waitForElement(nextStepDropDown);
   await I.click(nextStepDropDown);
   await I.see(eventDisplayName.UPDATE_LANG);
@@ -48,30 +48,35 @@ Scenario('Solicitor create case and make payment', async (I) => {
   await I.dontSee(eventDisplayName.REFUND);
   await I.dontSee(eventDisplayName.TRANSFER_BETWEEN_RDC);
   await I.dontSee(eventDisplayName.TRANSFER_CTSC_TO_RDC);
+  await I.click(signOut);
+}).retry(testconfig.TestRetryScenarios);
 
-  // Caseworker should be able to see issue, refund events and issue aos pack
-  // await I.amOnPage('/cases/case-details/' + caseNumber);
-  // await I.selectAndSubmitEvent(eventDisplayName.HWF_APP_ACCEPTED);
-  // await I.waitForElement(nextStepDropDown);
-  // await I.click(nextStepDropDown);
-  // await I.wait(0.5);
-  // await I.see(eventDisplayName.UPDATE_LANG);
-  // await I.see(eventDisplayName.ISSUE);
-  // await I.see(eventDisplayName.REFUND);
-  // await I.see(eventDisplayName.TRANSFER_BETWEEN_RDC);
-  // await I.see(eventDisplayName.TRANSFER_CTSC_TO_RDC);
-  // await I.issueFromSubmittedPageFormAndSubmit();
-  // await I.issueCheckYourAnswersPageFormAndSubmit();
-  // await I.selectAndSubmitEvent(eventDisplayName.ISSUE_AOS_TO_RESP);
-  // await I.selectEvent(eventDisplayName.AOS_STARTED);
-  // await I.aosStartedPageFormAndSubmit();
-  // await I.aosStartedCheckYourAnswersPageFormAndSubmit();
-  // await I.aosReceivedUndefendedMoveToDNFormSubmit();
-  // await I.selectAndSubmitEvent(eventDisplayName.DN_RECEIVED);
-  // await I.selectAndSubmitEvent(eventDisplayName.REFER_TO_LEGAL_ADVSIOR);
-  // await I.selectAndSubmitEvent(eventDisplayName.ENTITLEMENT_GRANTED);
-  // await I.selectAndSubmitEvent(eventDisplayName.DN_PRONOUNCED_BY_BULK);
-  // await I.setTestDataForDA();
-  // await I.selectAndSubmitEvent(eventDisplayName.MAKE_ELIGIBLE_FOR_DA);
-  // await I.selectAndSubmitEvent(eventDisplayName.DA_GRANTED);
+xScenario('Caseworker should be able to see issue, refund events and issue aos pack', async (I) => {
+  await I.amOnHomePage();
+  await I.login(testconfig.TestEnvCWUser, testconfig.TestEnvCWPassword);
+  await I.wait(0.5);
+  await I.amOnPage('/case/DIVORCE/DIVORCE/' + caseNumber);
+  await I.selectAndSubmitEvent(eventDisplayName.HWF_APP_ACCEPTED);
+  await I.waitForElement(nextStepDropDown);
+  await I.click(nextStepDropDown);
+  await I.wait(0.5);
+  await I.see(eventDisplayName.UPDATE_LANG);
+  await I.see(eventDisplayName.ISSUE);
+  await I.see(eventDisplayName.REFUND);
+  await I.see(eventDisplayName.TRANSFER_BETWEEN_RDC);
+  await I.see(eventDisplayName.TRANSFER_CTSC_TO_RDC);
+  await I.issueFromSubmittedPageFormAndSubmit();
+  await I.issueCheckYourAnswersPageFormAndSubmit();
+  await I.selectAndSubmitEvent(eventDisplayName.ISSUE_AOS_TO_RESP);
+  await I.selectEvent(eventDisplayName.AOS_STARTED);
+  await I.aosStartedPageFormAndSubmit();
+  await I.aosStartedCheckYourAnswersPageFormAndSubmit();
+  await I.aosReceivedUndefendedMoveToDNFormSubmit();
+  await I.selectAndSubmitEvent(eventDisplayName.DN_RECEIVED);
+  await I.selectAndSubmitEvent(eventDisplayName.REFER_TO_LEGAL_ADVSIOR);
+  await I.selectAndSubmitEvent(eventDisplayName.ENTITLEMENT_GRANTED);
+  await I.selectAndSubmitEvent(eventDisplayName.DN_PRONOUNCED_BY_BULK);
+  await I.setTestDataForDA();
+  await I.selectAndSubmitEvent(eventDisplayName.MAKE_ELIGIBLE_FOR_DA);
+  await I.selectAndSubmitEvent(eventDisplayName.DA_GRANTED);
 }).retry(testconfig.TestRetryScenarios);
