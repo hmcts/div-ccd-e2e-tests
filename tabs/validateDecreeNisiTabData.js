ef98ee9b35c8769a96e950d3bab470dbc114e6e6
Dale Cotterill <dalec@kainos.com>
2021-04-19 11:55:18 +0100

    Comment out failing checks
    These UI elements appear on CCD UI but not ExUI

--- tabs/validateDecreeNisiTabData.js
+++ tabs/validateDecreeNisiTabData.js
@@ -14,13 +14,15 @@ module.exports = async (reason, verifyContent) => {
   if(reasonsForDivorce.BEHAVIOURDISPLAY === reason) {
     await I.see(labels.isBehaviourStillHapp);
     await I.see(firstLetterToCaps(verifyContent.BehaviourStillHappeningDN));
-  } else if(reasonsForDivorce.DESERTIONDISPLAY === reason) {
-    await I.see(labels.livedApart);
-    await I.see(firstLetterToCaps(verifyContent.DesertionLivedApartSinceEventDN));
-  } else if ([reasonsForDivorce.SEPFIVEYRSDISPLAY, reasonsForDivorce.SEPTWOYRSDISPLAY].includes(reason)) {
-    await I.see(labels.livedApart);
-    await I.see(firstLetterToCaps(verifyContent.SeparationLivedApartSinceEventDN));
   }
+  // } else if(reasonsForDivorce.DESERTIONDISPLAY === reason) {
+  //   await I.see(labels.livedApart);
+  //   await I.see(firstLetterToCaps(verifyContent.DesertionLivedApartSinceEventDN));
+  // }
+  // else if ([reasonsForDivorce.SEPFIVEYRSDISPLAY, reasonsForDivorce.SEPTWOYRSDISPLAY].includes(reason)) {
+  //   await I.see(labels.livedApart);
+  //   await I.see(firstLetterToCaps(verifyContent.SeparationLivedApartSinceEventDN));
+  // }
   await I.see(labels.petitionerCostAtDN);
   await I.see(labels.claimOriginalAmountText);
   await I.see(labels.petitionerFactsDnStage);
