d6b66afa70a5298b247d8da09369f27f1c98f99c
Patryk Jar <jar.patryk@gmail.com>
2021-04-19 18:38:20 +0200

    we don't have case id in this step

--- tabs/validateLinkedCaseTabData.js
+++ tabs/validateLinkedCaseTabData.js
@@ -5,5 +5,5 @@ const labels = require('../data/tab-fields/linkedCase.json');
 module.exports = async () => {
   await I.clickTab(labels.name);
   await I.see(labels.previousCaseId);
-  await I.see('1515-7665-4180-5208');
+  // await I.see('1515-7665-4180-5208');
 };
