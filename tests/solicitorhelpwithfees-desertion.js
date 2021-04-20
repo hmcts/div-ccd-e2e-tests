d179aaa81a1fa854b0e490f5e92b9c8f815daa81
Adam Cox <8037782+adamjhc@users.noreply.github.com>
2021-04-20 16:53:10 +0100

    Remove desertion from name

--- tests/solicitorhelpwithfees-desertion.js
+++ tests/solicitorhelpwithfees-desertion.js
@@ -8,7 +8,7 @@ let caseNumber;
 
 Feature('Desertion');
 
-Scenario('Desertion Solicitor create case and make payment', async (I) => {
+Scenario('Solicitor create case and make payment', async (I) => {
   await I.amOnHomePage();
   await I.login(testconfig.TestEnvProfUser, testconfig.TestEnvProfPassword);
   await I.clickCreateCase();
