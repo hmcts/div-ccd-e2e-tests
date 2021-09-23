84751b02e6e8cd166463ef5ff2244418466ba70a
danielbe11 <daniel.bell@solirius.com>
2021-09-23 12:52:48 +0100

    Sign in wait

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -9,7 +9,7 @@ module.exports = {
   },
 
   async submitLogin(email, password) {
-    await I.waitForNavigation({ waitUntil: this.fields.email });
+    await I.waitForText('Sign in');
     await I.fillField(this.fields.email, email);
     await I.fillField(this.fields.password, password);
     await I.waitForNavigationToComplete(this.fields.submit);
