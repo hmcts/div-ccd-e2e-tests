64147aa5758c35dfb53236b7e28f8937550fc5df
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2021-11-01 18:12:45 +0000

    fixing issue on demo

--- pages/SolCaseCreatedPage.js
+++ pages/SolCaseCreatedPage.js
@@ -1,10 +1,12 @@
 const I = actor();
+const testConfig = require('../tests/config');
 
 module.exports = {
 
   fields: {
     selectActionDropDown: 'select[id="next-step"]',
     caseNumberDisplay: 'ccd-case-edit ccd-case-edit-page .heading-h2',
+    caseNumberDisplayDemo: 'ccd-case-edit ccd-case-edit-page ccd-markdown markdown p',
     submit: 'button[type="submit"]'
   },
 
@@ -12,8 +14,9 @@ module.exports = {
     await I.waitForElement(this.fields.selectActionDropDown);
     await I.selectOption(this.fields.selectActionDropDown, 'Case submission');
     await I.waitForNavigationToComplete(this.fields.submit);
-    await I.waitForElement(this.fields.caseNumberDisplay);
-    const display = await I.grabTextFrom(this.fields.caseNumberDisplay);
+    const displayField = testConfig.TestEnv === 'demo'? this.fields.caseNumberDisplayDemo: this.fields.caseNumberDisplay;
+    await I.waitForElement(displayField);
+    const display = await I.grabTextFrom(displayField);
     await I.wait(1);
     return display;
   }
