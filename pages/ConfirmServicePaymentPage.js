a7ea0441e19ca5395319a9ca1706ee624ad9a32b
prabhamuthu15 <praba.muthu@justice.gov.uk>
2022-01-20 11:23:49 +0000

    Fix AOSUndefendedEventSubmission

--- pages/ConfirmServicePaymentPage.js
+++ pages/ConfirmServicePaymentPage.js
@@ -13,10 +13,12 @@ module.exports = {
 
   async fillFormAndSubmit() {
     await I.selectOption(this.fields.selectActionDropDown, 'Confirm service payment');
-    await I.wait(5);
+    await I.wait(1);
     await I.waitForNavigationToComplete(this.fields.submit);
-    await I.wait(5);
-
+    await I.wait(1);
+    await I.waitForText('Total');
+    await I.waitForNavigationToComplete(this.fields.submit);
+    await I.wait(1);
     await I.waitForElement(this.fields.serviceApplicationPayment);
     await I.runAccessibilityTest();
     await I.selectOption(this.fields.serviceApplicationPayment, 'Help with Fees');
@@ -24,9 +26,11 @@ module.exports = {
     await I.waitForElement(this.fields.submit);
     await I.waitForNavigationToComplete(this.fields.submit);
 
-    await I.wait(3);
-    await I.waitForElement(this.fields.fieldsetCaseData);
-    await I.waitForNavigationToComplete(this.fields.submit);
+    //await I.wait(3);
+    //await I.waitForElement(this.fields.fieldsetCaseData);
+    //await I.waitForNavigationToComplete(this.fields.submit);
+
+
 
     await I.wait(3);
     await I.waitForElement(this.fields.eventSummary);
